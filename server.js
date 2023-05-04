57a2a1f6ebd536e66baa3181e70a0b9f57be450d
Aditya Gurjar <17155247+adityadroid@users.noreply.github.com>
2023-05-04 23:26:40 +0530

    add compression

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const http = require('http');
+const zlib = require('zlib'); // Require the zlib module
 const mediumJSONFeed = require('./index.js');
 
 const port = process.env.PORT || 3000;
 const headers = {
   'Content-Type': 'application/json',
-  'Access-Control-Allow-Origin': '*'
+  'Access-Control-Allow-Origin': '*',
+  'Content-Encoding': 'gzip' // Add the Content-Encoding header to indicate gzip compression
 };
 
 http.createServer((req, res) => {
@@ -17,7 +19,8 @@ http.createServer((req, res) => {
 
   mediumJSONFeed(req.url, data => {
     res.writeHead(data.status || 500, headers);
-    res.end(JSON.stringify(data, null, 2), 'utf-8');
+    const gzip = zlib.createGzip(); // Create a gzip transform stream
+    data.pipe(gzip).pipe(res); // Use the gzip stream to compress the response data
   });
 
 }).listen(port);
